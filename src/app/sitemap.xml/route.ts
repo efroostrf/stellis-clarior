ec6f8ad4f9c1e6b80b7d75b879e9e062177cca95
Yefrosynii <contact@yefro.dev>
2024-01-28 22:09:04 +0000

    feat: sitemap and robots

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,67 @@
+import { eq } from "drizzle-orm";
+import {
+  EnumChangefreq,
+  SitemapItem,
+  SitemapStream,
+  streamToPromise,
+} from "sitemap";
+import { Readable } from "stream";
+
+import { db } from "@/drizzle/db";
+import { posts } from "@/drizzle/schema";
+
+export const dynamic = "force-dynamic";
+
+const constantPages: Partial<SitemapItem>[] = [
+  {
+    url: "/",
+    priority: 1,
+    changefreq: EnumChangefreq.DAILY,
+  },
+  {
+    url: "/blog",
+    priority: 0.6,
+    changefreq: EnumChangefreq.DAILY,
+  },
+];
+
+export const GET = async () => {
+  const postsData = await db.query.posts.findMany({
+    where: eq(posts.isDraft, false),
+  });
+
+  const stream = new SitemapStream({
+    hostname: "https://hinkali.city/",
+  });
+
+  const write = (item: Partial<SitemapItem>) => stream.write(item);
+
+  // Write constant pages
+  constantPages.forEach(write);
+
+  // Write pages
+  for (const { slug, updatedAt } of postsData) {
+    write({
+      url: `/blog/${slug}`,
+      priority: 0.8,
+      changefreq: EnumChangefreq.DAILY,
+      lastmod: new Date(updatedAt).toISOString(),
+    });
+  }
+
+  // End stream of sitemap
+  stream.end();
+
+  // Ready to render xml for client
+  const xml = await streamToPromise(Readable.from(stream)).then((data) =>
+    data.toString()
+  );
+
+  const res = new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+
+  return res;
+};
