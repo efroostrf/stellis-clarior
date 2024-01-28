d8470970fc60de6922adf7c8c07ebc0b43f2117b
Yefrosynii <contact@yefro.dev>
2024-01-28 21:45:31 +0000

    feat: blog post metadata

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import { eq } from "drizzle-orm";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { FC } from "react";
 
@@ -9,7 +10,39 @@ import { posts } from "@/drizzle/schema";
 
 import { BlogPostRenderer } from "./_components/renderer";
 
-const BlogPostPage: FC<{ params: { slug: string } }> = async (props) => {
+type Props = { params: { slug: string } };
+
+export const generateMetadata = async (props: Props): Promise<Metadata> => {
+  const slug = props.params.slug;
+
+  const isAdmin = await isAdminAction();
+  const post = await db.query.posts.findFirst({
+    where: eq(posts.slug, slug),
+  });
+
+  if (!post || (post.isDraft && !isAdmin)) {
+    return notFound();
+  }
+
+  return {
+    title: `${post.title} - Блог Даны Фалевой - Кинолог`,
+    description: "",
+    keywords: post.tags.split(","),
+    openGraph: {
+      title: `${post.title} - Блог Даны Фалевой - Кинолог`,
+      description: "",
+      type: "website",
+      url: `https://stellisclarior.com/blog/${post.slug}`,
+      locale: "ru_RU",
+      siteName: "Ярче звезд - Дана Фалева",
+    },
+    alternates: {
+      canonical: `/blog/${post.slug}`,
+    },
+  };
+};
+
+const BlogPostPage: FC<Props> = async (props) => {
   const slug = props.params.slug;
 
   const isAdmin = await isAdminAction();
